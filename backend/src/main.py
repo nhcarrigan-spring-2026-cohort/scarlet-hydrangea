22b2ce95922f20e35bd575f7c0774999457a4151
Sebastian Włodarczyk <sebastianwlodarczyk92@protonmail.com>
2026-02-03 01:33:08 +0100

    feat: create backend boilerplate
    This replaces the default Python boilerplate created by `uv init` with a basic Flask web application.

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,6 +1,11 @@
-def main():
-    print("Hello from backend!")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "<p>Hello from backend!</p>"
 
 
 if __name__ == "__main__":
-    main()
+    app.run(host="0.0.0.0", port=8000, debug=True)
