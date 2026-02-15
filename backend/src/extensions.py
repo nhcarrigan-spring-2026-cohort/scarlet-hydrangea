e4ae8b69e38b9aef524789972f013f616680d86f
Sebastian Włodarczyk <sebastianwlodarczyk92@protonmail.com>
2026-02-15 04:57:58 +0100

    chore: added 'CORS' to the list of backend dependencies
    This adds 'CORS' from 'flask_cors' to the list of dependinecies for the Flask app.

--- backend/src/extensions.py
+++ backend/src/extensions.py
@@ -1,11 +1,14 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_jwt_extended import JWTManager
+from flask_cors import CORS
 
 # Initialize extensions (no app reference yet - prevents circular imports)
 db = SQLAlchemy()
 migrate = Migrate()
 jwt = JWTManager()
+cors = CORS()
+
 
 
 def init_extensions(app):
@@ -13,3 +16,4 @@ def init_extensions(app):
     db.init_app(app)
     migrate.init_app(app, db)
     jwt.init_app(app)
+    cors.init_app(app)
