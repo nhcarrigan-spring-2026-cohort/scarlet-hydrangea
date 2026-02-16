881ac864eddaab8251634d361d7a1798facc1ced
Sinan Farook <134277290+SinanFarook@users.noreply.github.com>
2026-02-16 19:27:20 +0300

    chore: initialize marshmallow object

--- backend/src/extensions.py
+++ backend/src/extensions.py
@@ -1,11 +1,13 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_jwt_extended import JWTManager
+from flask_marshmallow import Marshmallow
 
 # Initialize extensions (no app reference yet - prevents circular imports)
 db = SQLAlchemy()
 migrate = Migrate()
 jwt = JWTManager()
+ma = Marshmallow()
 
 
 def init_extensions(app):
@@ -13,3 +15,4 @@ def init_extensions(app):
     db.init_app(app)
     migrate.init_app(app, db)
     jwt.init_app(app)
+    ma.init_app(app)
