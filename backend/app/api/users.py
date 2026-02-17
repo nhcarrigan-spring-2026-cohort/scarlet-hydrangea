023100b1ff57d09deac9337d1e9dd9534fa4f896
Sebastian Włodarczyk <sebastianwlodarczyk92@protonmail.com>
2026-02-17 03:35:04 +0100

    chore: updated '/api/users/<id>' GET response to retuen data from the Schema
    This changes the '/api/users/<id>' response to return data processed by Marshmallow.

--- backend/app/api/users.py
+++ backend/app/api/users.py
@@ -3,11 +3,21 @@ from app.crud import create_user, get_all_users, get_user
 
 users_bp = Blueprint('users', __name__)
 
+'''
+/api/users route
+'''
 @users_bp.route('/users', methods=['GET'])
 def get_users_endpoint():
     users = get_all_users()
     return jsonify(users), 200
-    
+
+@users_bp.route('/users/<int:user_id>', methods=['GET'])
+def get_user_endpoint(user_id):
+    user = get_user(user_id)
+    if not user:
+        return jsonify({'error': 'Not found'}), 404
+    return jsonify(user), 200
+
 @users_bp.route('/users', methods=['POST'])
 def create_user_endpoint():
     data = request.get_json()
@@ -16,10 +26,3 @@ def create_user_endpoint():
         'id': user.id, 'email': user.email
     }), 201
 
-
-@users_bp.route('/users/<int:user_id>', methods=['GET'])
-def get_user_endpoint(user_id):
-    user = get_user(user_id)
-    if not user:
-        return jsonify({'error': 'Not found'}), 404
-    return jsonify({'id': user.id, 'email': user.email}), 200
