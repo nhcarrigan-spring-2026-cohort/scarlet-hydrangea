3290f2048d489ee91693a1d498d5be79e8b70202
Sebastian Włodarczyk <sebastianwlodarczyk92@gmail.com>
2026-02-17 21:27:03 +0100

    refactor: updated '/api/tools' endpoints to use schemas
    This updates the '/api/tools' GET and POST routes to use Marshmallow schemas for data serialization and validation.

--- backend/app/api/items.py
+++ backend/app/api/items.py
@@ -1,62 +1,55 @@
 from flask import Blueprint, jsonify, request
+from marshmallow import ValidationError
+from sqlalchemy.exc import IntegrityError
+from src import db
 from app.crud import get_all_items, add_item, get_item_by_id
+from app.schemas import ItemSchema
 
 items_bp = Blueprint('items', __name__)
 
+'''
+/api/items route
+'''
 @items_bp.route('/tools', methods=['GET'])
 def get_items_endpoint():
     items = get_all_items()
-    response = {'tools': [
-        {
-            'id': item.id,
-            'name':item.name,
-            'description': item.description,
-            'category': item.category,
-            'owner_id': item.owner_id,
-            'available': item.is_available,
-            'total_quantity': item.total_quantity,
-            'available_quantity': item.available_quantity,
-            'condition': item.condition,
-            'created_at': item.created_at,
-            'updated_at': item.updated_at
-        } for item in items]
-    }
-    print(response, items)
-    if not items:
-        return jsonify({'error': 'Not found'}), 404
-    return jsonify(response), 200
+    schema = ItemSchema(many=True)
+    return jsonify({"tools": schema.dump(items)}), 200
 
 @items_bp.route('/tools', methods=['POST'])
 def add_item_endpoint():
-    data = request.get_json()
-    item = add_item(
-        data['name'],
-        data['description'],
-        data['category'],
-        data['owner_id'],
-        data['quantity'],
-        data['condition']
-    )
-    return jsonify({
-        'id': item.id,
-        'name': item.name,
-        'category': item.category,
-        'owner_id': item.owner_id,
-        'condition': item.condition
-    })
+    raw_data = request.get_json()
+    
+    if not raw_data:
+        return jsonify({'error': 'Request body is required'}), 400      # Bad Request
+    
+    schema = ItemSchema()
+    try:
+        data = schema.load(raw_data)
+    except ValidationError as err:
+        return jsonify({'error': err.messages}), 400
+    try:
+        item = add_item(
+            raw_data['name'],
+            raw_data['description'],
+            raw_data['category'],
+            raw_data['owner_id'],
+            raw_data['total_quantity'],
+            raw_data['condition']
+        )
+    except IntegrityError:
+        db.session.rollback()
+        return jsonify({'error': 'something went wrong'}), 409
+    except TypeError:
+        return jsonify({"error": "input data is not correct type"}), 400      # Bad Request
+    
+    return jsonify(ItemSchema().dump(item)), 201
 
 @items_bp.route('/tools/<int:item_id>', methods=['GET'])
 def get_item_endpoint(item_id):
     item = get_item_by_id(item_id)
     if not item:
-        return jsonify({'error': 'Not found'}), 404
-    return jsonify({
-        'id': item.id,
-        'name': item.name,
-        'description': item.description,
-        'category': item.category,
-        'available': item.is_available,
-        'condition': item.condition,
-        'owner_id': item.owner_id
-        }), 200
+        return jsonify({'error': 'Tool not found'}), 404
+    schema = ItemSchema()
+    return jsonify(schema.dump(item)), 200
     
