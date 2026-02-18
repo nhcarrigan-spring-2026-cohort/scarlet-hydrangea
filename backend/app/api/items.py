0776e81946dee62564f3cc370c695ce9762cd0f1
Sebastian Włodarczyk <sebastianwlodarczyk92@protonmail.com>
2026-02-18 16:04:14 +0100

    refactor: applied changes suggested in the review

--- backend/app/api/items.py
+++ backend/app/api/items.py
@@ -1,20 +1,20 @@
 from flask import Blueprint, jsonify, request
 from marshmallow import ValidationError
 from sqlalchemy.exc import IntegrityError
-from src import db
+from src.extensions import db
 from app.crud import get_all_items, add_item, get_item_by_id
-from app.schemas import ItemSchema
+from app.schemas import ItemSchema, ItemValidationSchema
 
 items_bp = Blueprint('items', __name__)
 
 '''
-/api/items route
+/api/tools route
 '''
 @items_bp.route('/tools', methods=['GET'])
 def get_items_endpoint():
     items = get_all_items()
     schema = ItemSchema(many=True)
-    return jsonify({"tools": schema.dump(items)}), 200
+    return jsonify(schema.dump(items)), 200
 
 @items_bp.route('/tools', methods=['POST'])
 def add_item_endpoint():
@@ -23,25 +23,23 @@ def add_item_endpoint():
     if not raw_data:
         return jsonify({'error': 'Request body is required'}), 400      # Bad Request
     
-    schema = ItemSchema()
+    schema = ItemValidationSchema()
     try:
         data = schema.load(raw_data)
     except ValidationError as err:
         return jsonify({'error': err.messages}), 400
     try:
         item = add_item(
-            raw_data['name'],
-            raw_data['description'],
-            raw_data['category'],
-            raw_data['owner_id'],
-            raw_data['total_quantity'],
-            raw_data['condition']
+            data['name'],
+            data['description'],
+            data['category'],
+            data['owner_id'],
+            data['total_quantity'],
+            data['condition']
         )
     except IntegrityError:
         db.session.rollback()
-        return jsonify({'error': 'something went wrong'}), 409
-    except TypeError:
-        return jsonify({"error": "input data is not correct type"}), 400      # Bad Request
+        return jsonify({'error': 'Invalid owner_id - user does not exist'}), 400      # Bad Request - user id not found
     
     return jsonify(ItemSchema().dump(item)), 201
 
