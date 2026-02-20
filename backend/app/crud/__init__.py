d1599e3ef6bc55f9aa9c08df8c9022d8079fb928
Sebastian Włodarczyk <sebastianwlodarczyk92@protonmail.com>
2026-02-20 03:06:19 +0100

    feat: add 'add_item', 'get_item' and 'get_all_items' CRUD operations
    This adds CRUD operations allowing to read all items (tools) in a database, or a specific item selected by its id fron the 'items' table, as well as creating adding new items to the database.

--- backend/app/crud/__init__.py
+++ backend/app/crud/__init__.py
@@ -1,4 +1,5 @@
 """CRUD operations package"""
 from app.crud.user import get_all_users, get_user
+from app.crud.item import get_all_items ,get_item_by_id, add_item
 
-__all__ = ['get_all_users', 'get_user', 'create_user']
+__all__ = ['get_all_users', 'get_user', 'create_user', 'get_all_items' ,'get_item_by_id', 'add_item']
---
+++ backend/app/crud/item.py
@@ -0,0 +1,29 @@
+from src.extensions import db
+from app.models.item import Item
+from sqlalchemy import select
+
+def add_item(name, description, category, owner_id, total_quantity, condition):
+    """
+    Creates a new item (tool)
+    """
+    item = Item(
+        name = name,
+        description = description,
+        category = category,
+        owner_id = owner_id,
+        total_quantity = total_quantity,
+        available_quantity = total_quantity,
+        condition = condition
+    )
+    db.session.add(item)
+    db.session.commit()
+    return item
+
+def get_item_by_id(item_id: int):
+    """Return item by ID"""
+    return db.session.get(Item, item_id)
+
+def get_all_items():
+    """Return a list of all items"""
+    stmt = select(Item)
+    return db.session.execute(stmt).scalars().all()
