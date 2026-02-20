3947e83e71d8e30b65b8890c87d2908888197f5a
Sebastian Włodarczyk <sebastianwlodarczyk92@protonmail.com>
2026-02-20 03:07:55 +0100

    feat: add initial 'tools' endpoints
    This adds 3 new api endpoints for getting a list of all items (tools), item by its id number, and for adding new items to the database.

--- backend/app/api/__init__.py
+++ backend/app/api/__init__.py
@@ -1,5 +1,6 @@
 """API blueprints"""
 from app.api.health import health_bp
 from app.api.users import users_bp
+from app.api.items import items_bp
 
-__all__ = ['health_bp', 'users_bp']
+__all__ = ['health_bp', 'users_bp', 'items_bp']
