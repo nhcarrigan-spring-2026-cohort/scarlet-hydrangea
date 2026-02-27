b4e838582cc593b497fc87875954cfcfe59bda0d
Sinan Farook <134277290+SinanFarook@users.noreply.github.com>
2026-02-27 14:57:50 +0300

    fix(models): add index and default revoked_at time
    added index=True for jti for faster db search
    added server default timestamp for revoked_at time

--- backend/app/models/revoked_token.py
+++ backend/app/models/revoked_token.py
@@ -8,9 +8,9 @@ class RevokedToken(db.Model):
     __tablename__ = 'revoked_tokens'
     
     id = db.Column(db.Integer, primary_key=True)
-    jti = db.Column(db.String(36), nullable=False)
-    revoked_at = db.Column(db.DateTime(timezone=True), nullable=False)
-    delete_at = db.Column(db.DateTime(timezone=True), nullable=False)
+    jti = db.Column(db.String(36), nullable=False, index=True)
+    revoked_at = db.Column(db.DateTime(timezone=True), nullable=False, server_default=db.func.now())
+    expires_at = db.Column(db.DateTime(timezone=True), nullable=False)
     
     def __repr__(self):
         return f'<Token {self.jti}>'
