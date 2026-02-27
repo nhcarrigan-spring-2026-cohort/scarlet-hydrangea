9bb80a05f2f425c19b78f141b59d24167de66c11
Sinan Farook <134277290+SinanFarook@users.noreply.github.com>
2026-02-27 15:48:35 +0300

    feat: add callback fn to check if token revoked

--- backend/app/crud/__init__.py
+++ backend/app/crud/__init__.py
@@ -5,4 +5,4 @@ from app.crud.borrow import get_all_borrows, get_borrow, get_borrows_by_user, cr
 from app.crud.revoked_token import add_revoked_token, get_revoked_token
 
 __all__ = ['get_all_users', 'get_user', 'create_user', 'get_all_items' ,'get_item_by_id', 'add_item',
-           'get_all_borrows', 'get_borrow','get_borrows_by_user', 'create_borrow_request', 'approve_borrow', 'return_borrow', 'add_revoked_token', 'get_revoked_token']
+           'get_all_borrows', 'get_borrow','get_borrows_by_user', 'create_borrow_request', 'approve_borrow', 'return_borrow', 'add_revoked_token', 'is_revoked_token']
